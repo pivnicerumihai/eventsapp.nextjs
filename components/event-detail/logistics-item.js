97f1447475911323ff197345e600da39fcff0117
Mihai Pivniceru <mcpivniceru@gmail.com>
2021-04-20 20:55:10 +0300

    updated dynamic route page eventid

---
+++ components/event-detail/logistics-item.js
@@ -0,0 +1,16 @@
+import classes from './logistics-item.module.css';
+
+function LogisticsItem({ icon: Icon, children }) {
+
+
+  return (
+    <li className={classes.item}>
+      <span className={classes.icon}>
+        <Icon />
+      </span>
+      <span className={classes.content}>{children}</span>
+    </li>
+  );
+}
+
+export default LogisticsItem;
--- pages/events/[eventid].js
+++ pages/events/[eventid].js
@@ -1,9 +1,36 @@
+import { useRouter } from 'next/router';
+
+import EventSummary from '../../components/event-detail/event-summary';
+import EventLogistics from '../../components/event-detail/event-logistics';
+import EventContent from '../../components/event-detail/event-content';
+
+import { getEventById } from '../../dummy-data';
+
 function EventDetailPage(){
+
+    const router = useRouter();
+    const eventId = router.query.eventid;
+    const event = getEventById(eventId);
+
+    if(!event){
+        return <p> No event found!</p>
+    }
+    else{
     return(
-        <div>
-            <h1>Event Detail Page</h1>
-        </div>
+        <>
+        <EventSummary title={event.title}/>
+        <EventLogistics 
+        date={event.date}
+        address={event.location}
+        image={event.image}
+        imageAlt={event.title}/>
+        
+        <EventContent>
+        <p>{event.description}</p>
+        </EventContent>
+        </>
     )
+    }
 }
 
 export default EventDetailPage;
